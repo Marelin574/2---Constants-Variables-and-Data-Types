870db60108514e6545f66e598fe4b83c87cd3e92
Mason Guaman <masonstalin@gmail.com>
2023-09-11 11:11:05 -0400

    Initial Commit

---
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+## Exercise - Constants
+
+ Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
+ */
+let friends = 1000
+print(friends)
+
+
+
+//:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
+
+
+/*:
+page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
+ */
